a09644929016ece6a61c7c500045fcb057d301a3
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-09-02 09:15:30 +0200

    Add exports to index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import createForm from './createForm';
+import {
+  createFormSubmitAction,
+  createFormValidationAction
+} from './createFormActions';
+import generateInputField from 'components/generateInputField';
+
+
+export {
+  createForm,
+  createFormActions,
+  createFormValidationAction,
+  generateInputField,
+};
