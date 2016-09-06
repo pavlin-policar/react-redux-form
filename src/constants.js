af3a2cec5824a56419349d5a53cdb8b8ee3fddb2
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-09-06 18:50:31 +0200

    Add @@ to contants

--- src/constants.js
+++ src/constants.js
@@ -2,23 +2,24 @@
  * Todos constants
  */
 
-export const REGISTER_FORM = 'Form.REGISTER_FORM';
-export const UNREGISTER_FORM = 'Form.UNREGISTER_FORM';
+export const REGISTER_FORM = '@@Form.REGISTER_FORM';
+export const UNREGISTER_FORM = '@@Form.UNREGISTER_FORM';
 
-export const ATTACH_TO_FORM = 'Form.ATTACH_TO_FORM';
-export const DETACH_FROM_FORM = 'Form.DETACH_FROM_FORM';
+export const ATTACH_TO_FORM = '@@Form.ATTACH_TO_FORM';
+export const DETACH_FROM_FORM = '@@Form.DETACH_FROM_FORM';
 
-export const CHANGE = 'Form.CHANGE';
+export const CHANGE = '@@Form.CHANGE';
 
-export const FOCUS = 'Form.FOCUS';
-export const BLUR = 'Form.BLUR';
-export const TOUCH = 'Form.TOUCH';
+export const FOCUS = '@@Form.FOCUS';
+export const BLUR = '@@Form.BLUR';
+export const TOUCH = '@@Form.TOUCH';
 
-export const RESET = 'Form.RESET';
+export const RESET = '@@Form.RESET';
 
-export const SUBMIT_REQUEST = 'Form.SUBMIT_REQUEST';
-export const SUBMIT_SUCCESS = 'Form.SUBMIT_SUCCESS';
-export const SUBMIT_FAILURE = 'Form.SUBMIT_FAILURE';
+export const SUBMIT_REQUEST = '@@Form.SUBMIT_REQUEST';
+export const SUBMIT_SUCCESS = '@@Form.SUBMIT_SUCCESS';
+export const SUBMIT_FAILURE = '@@Form.SUBMIT_FAILURE';
 
-export const VALIDATION_REQUEST = 'Form.VALIDATION_REQUEST';
-export const RECEIVE_VALIDATION_ERRORS = 'Form.RECEIVE_VALIDATION_ERRORS';
+export const VALIDATION_REQUEST = '@@Form.VALIDATION_REQUEST';
+export const VALIDATION_NO_ERRORS = '@@Form.VALIDATION_NO_ERRORS';
+export const VALIDATION_ERRORS = '@@Form.VALIDATION_ERRORS';
